a0c0a8d1651548802760cd62448428ed8a8372b9
VarakanthamSaiHruthikReddy975 <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2024-01-11 00:26:06 -0600

    First commit

---
+++ src/test/java/com/vshr/springjenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vshr.springjenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
